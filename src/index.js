17ae34e4799c55b2e181104b583948cdf595240e
AnnaFesenko7 <annafecenko7@gmail.com>
2022-06-23 20:06:08 +0200

    change basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/AnnaFesenko7/">
+    <BrowserRouter basename="/AnnaFesenko7/react-router/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
