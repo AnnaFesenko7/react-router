b3f4ddf71f73bfb7e3c69d553e2ad852ef8eb4e7
AnnaFesenko7 <annafecenko7@gmail.com>
2022-06-23 20:34:17 +0200

    fix repo-name

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/AnnaFesenko7/react-router/">
+    <BrowserRouter basename="/react-router/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
