854bec74ab9f9f572274b6d2d7d5d93785e555ad
AnnaFesenko7 <annafecenko7@gmail.com>
2023-05-31 22:02:09 +0300

    createAsyncThunk

--- src/redux/books/booksOperaations.js
+++ src/redux/books/booksOperaations.js
@@ -1,12 +1,26 @@
-import * as bookActions from './bookActions';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+// import * as bookActions from './bookActions';
 import * as bookShelf from 'services/bookshelf-api';
 
-export const fetchBooks = () => async dispatch => {
-  dispatch(bookActions.fetchBooksRequest());
-  try {
-    const books = await bookShelf.fetchBooks();
-    dispatch(bookActions.fetchBooksSuccess(books));
-  } catch (error) {
-    dispatch(bookActions.fetchBooksError(error));
+// export const fetchBooks = () => async dispatch => {
+//   dispatch(bookActions.fetchBooksRequest());
+//   try {
+//     const books = await bookShelf.fetchBooks();
+//     dispatch(bookActions.fetchBooksSuccess(books));
+//   } catch (error) {
+//     dispatch(bookActions.fetchBooksError(error));
+//   }
+// };
+
+export const fetchBooks = createAsyncThunk(
+  'books/fetchBooks',
+  async (_, { rejectWithValue }) => {
+    try {
+      const books = await bookShelf.fetchBooks();
+      return books;
+    } catch (err) {
+      return rejectWithValue(err);
+    }
   }
-};
+);
--- src/redux/books/booksReducer.js
+++ src/redux/books/booksReducer.js
@@ -1,19 +1,26 @@
 import { createReducer, combineReducers } from '@reduxjs/toolkit';
-import * as bookActions from './bookActions';
+// import * as bookActions from './bookActions';
+import { fetchBooks } from './booksOperaations';
 
 const entities = createReducer([], {
-  [bookActions.fetchBooksSuccess]: (_, action) => action.payload,
+  // [bookActions.fetchBooksSuccess]: (_, action) => action.payload,
+  [fetchBooks.fulfilled]: (_, action) => action.payload,
 });
 
 const isLoading = createReducer(false, {
-  [bookActions.fetchBooksRequest]: () => true,
-  [bookActions.fetchBooksSuccess]: () => false,
-  [bookActions.fetchBooksError]: () => false,
+  // [bookActions.fetchBooksRequest]: () => true,
+  // [bookActions.fetchBooksSuccess]: () => false,
+  // [bookActions.fetchBooksError]: () => false,
+  [fetchBooks.pending]: () => true,
+  [fetchBooks.fulfilled]: () => false,
+  [fetchBooks.rejected]: () => false,
 });
 
 const error = createReducer(null, {
-  [bookActions.fetchBooksError]: (_, action) => action.payload,
-  [bookActions.fetchBooksRequest]: () => null,
+  // [bookActions.fetchBooksError]: (_, action) => action.payload,
+  // [bookActions.fetchBooksRequest]: () => null,
+  [fetchBooks.rejected]: (_, action) => action.payload,
+  [fetchBooks.pending]: () => null,
 });
 
 export const booksReducer = combineReducers({
